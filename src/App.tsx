066833632be6d74569a10a0ee4fca3ce78fa0473
Aden Fernandes <adenferns999@gmail.com>
2023-12-07 22:41:10 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes,  Route } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import './globals.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import { Toaster } from "@/components/ui/toaster"
+
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+        {/* Public Route */}
+        <Route element={<AuthLayout />}>
+        <Route path='/sign-in' element={<SigninForm />} />
+        <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+
+        {/* Private Route */}
+        <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        </Route>
+        </Routes>
+
+        <Toaster />
+    </main>
+  )
+}
+
+export default App
