936623b234ff346739b2fa4b97eccca7ef4259c0
Aden Fernandes <adenferns999@gmail.com>
2023-12-15 18:40:33 +0530

    feat: Create Post functionality added
    - Added React-dropzone to handle fileupaloding
    - Updated the Post form.
    - Added File storage calls to Api.ts
    - Added Query Keys file

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,7 @@
-import { useQuery, useMutation, useInfiniteQuery, useQueryClient, Mutation } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { useQuery, useMutation, useInfiniteQuery, useQueryClient, Mutation, QueryClient } from "@tanstack/react-query";
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
+import { QUERY_KEYS } from "./querykeys";
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -20,3 +21,23 @@ export const useCreateUserAccount = () => {
       mutationFn: signOutAccount,
     });
   };
+
+  export const useCreatePost= () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post), 
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+        })
+      }
+    })
+  }
+
+  export const useGetRecentPosts = () => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      queryFn: getRecentPosts
+    })
+  }
--- src/lib/validations/index.ts
+++ src/lib/validations/index.ts
@@ -11,3 +11,10 @@ export const SignupValidation= z.object({
     email: z.string().email(),
     password: z.string().min(8, { message: "Password must be at least 8 characters." }),
   })
+
+  export const PostValidation= z.object({
+    caption: z.string().min(5, {message: "Please enter a caption longer than 5 characters"}).max(2200, {message: "Cannot exceed 2200 characters"}),
+    file: z.custom<File[]>(),
+    location: z.string().min(2, {message:  "Please enter a location"}).max(1000 , {message: "Location cannot exceed 1000 characters"}),
+    tags: z.string()
+  })
